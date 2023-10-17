ecaf0ece120e0667bf2eba6f32c0ded042ccd291
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-17 16:58:34 +0545

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -22,7 +22,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             ''.join(["►" for i in range(math.floor(percentage / 5))]),
             ''.join(["▻" for i in range(20 - math.floor(percentage / 5))])
         )            
-        tmp = progress + rkn.PROGRESS_BAR.format( 
+        tmp = progress + rkn.RKN_PROGRESS.format( 
             round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
