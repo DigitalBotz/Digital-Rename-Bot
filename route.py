32c1c80847101316af1529c3001b8a53f299ea1a
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-17 16:32:18 +0545

    Update route.py

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("PyʀᴏBᴏᴛᴢ")
+    return web.json_response("RknDeveloper")
 
 
 async def web_server():
