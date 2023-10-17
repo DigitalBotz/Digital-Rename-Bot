f9949e144727f56ad37b730e04dec56fa56dee37
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-17 16:33:51 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ class Bot(Client):
         if Config.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
+            await web.TCPSite(app, "0.0.0.0", 8000).start()     
         print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
