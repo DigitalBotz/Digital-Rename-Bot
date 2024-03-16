b951c266978bea746743fa80159d50a15f164950
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-03-16 10:34:42 +0545

    Update To V2.0.0 🔥

--- route.py
+++ route.py
@@ -1,13 +1,13 @@
 from aiohttp import web
 
-routes = web.RouteTableDef()
+Rkn_FileRenameBot = web.RouteTableDef()
 
-@routes.get("/", allow_head=True)
+@Rkn_FileRenameBot.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("RknDeveloper")
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
+    web_app.add_routes(Rkn_FileRenameBot)
     return web_app
