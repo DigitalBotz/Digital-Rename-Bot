c6e02d72af12f25d8547b60b9ac9da950bafa9f3
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-04-01 09:43:51 +0545

    Update To V2.0.5 🔥

--- bot.py
+++ bot.py
@@ -1,22 +1,24 @@
-from datetime import datetime
-from pytz import timezone
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+
+import aiohttp, pytz, datetime, logging, logging.config
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from config import Config
-from aiohttp import web
-from route import web_server
-import logging.config
-import logging
+from plugins.web_support import web_server
 
 # Get logging configurations
 logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 
-class Bot(Client):
+class Rkn_FileRenameBot(Client):
     def __init__(self):
         super().__init__(
-            name="tg-filerenamebot",
+            name="Rkn_FileRenameBot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
@@ -29,22 +31,31 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            app = web.AppRunner(await web_server())
-            await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8000).start()     
+        self.uptime = Config.BOT_UPTIME
+        app = aiohttp.web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await aiohttp.web.TCPSite(app, bind_address, Config.PORT).start()
         print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
             except: pass
         if Config.LOG_CHANNEL:
             try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
+                curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
                 date = curr.strftime('%d %B, %Y')
                 time = curr.strftime('%I:%M:%S %p')
                 await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
             except:
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
-Bot().run()
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot Stopped 🙄")
+
+Rkn_FileRenameBot().run()
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
