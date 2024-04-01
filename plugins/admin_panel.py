7033a385e83b3882b0f0d95bc01e494461cf922a
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-04-01 07:34:42 +0545

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -40,7 +40,9 @@ async def restart_bot(b, m):
     all_users = await db.get_all_users()
     async for user in all_users:
         try:
-            rkn = await send_msg(user['_id'], f"**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
+            msg = f"ʜᴇʏ, {(await b.get_users(user['_id'])).mention}\n\n**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**"
+            restart_msg = m.msg
+            rkn = await send_msg(user['_id'], restart_msg)
             if rkn == 200:
                 success += 1
             else:
