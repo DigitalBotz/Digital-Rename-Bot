a2a5b2a27a684cd31f7d727a0f71f3b1be62a870
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-04-01 09:34:49 +0545

    Update To V2.0.5 🔥

---
+++ plugins/web_support.py
@@ -0,0 +1,23 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+
+from aiohttp import web
+
+Rkn_FileRenameBot = web.RouteTableDef()
+
+@Rkn_FileRenameBot.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("RknDeveloper")
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(Rkn_FileRenameBot)
+    return web_app
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
