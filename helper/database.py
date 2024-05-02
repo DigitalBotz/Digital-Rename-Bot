c6a0c261add6b2e27432a067a0b72078e712922e
JayMahakal98 <136795836+JayMahakal98@users.noreply.github.com>
2024-05-02 07:08:12 +0545

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,19 +1,31 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+# Special Thanks To (https://github.com/JayMahakal98)
+
 import motor.motor_asyncio
 from config import Config
 from .utils import send_log
 
 class Database:
-
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.user
+        self.premium = self.db.premium
 
     def new_user(self, id):
         return dict(
             _id=int(id),                                   
             file_id=None,
-            caption=None
+            caption=None,
+            prefix=None,
+            suffix=None,
+            metadata_mode=False,
+            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Rkn_Bots" -metadata author="@RknDeveloper" -metadata:s:s title="Subtitled By :- @Rkn_Bots" -metadata:s:a title="By :- @RknDeveloper" -metadata:s:v title="By:- @Rkn_Bots" """,
+            expiry_time=None
         )
 
     async def add_user(self, b, m):
@@ -52,9 +64,72 @@ class Database:
         user = await self.col.find_one({'_id': int(id)})
         return user.get('caption', None)
 
+    async def set_prefix(self, id, prefix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})
 
+    async def get_prefix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('prefix', None)
+
+    async def set_suffix(self, id, suffix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})
+
+    async def get_suffix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('suffix', None)
+
+    async def set_metadata_mode(self, id, bool_meta):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_mode': bool_meta}})
+
+    async def get_metadata_mode(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_mode', None)
+
+    async def set_metadata_code(self, id, metadata_code):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
+
+    async def get_metadata_code(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_code', None)
+
+    async def addpremium(self, user_id, user_data):    
+        await self.premium.update_one({"id": user_id}, {"$set": user_data}, upsert=True)
+
+    async def remove_premium(self, user_id):
+        await self.premium.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
+
+    async def checking_remaining_time(self, id):
+        user = await self.premium.find_one({'id': int(id)})
+        return user.get('expiry_time', None)
+
+    async def get_user(self, user_id):
+        user_data = await self.premium.find_one({"id": user_id})
+        return user_data
+        
+    async def has_premium_access(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            expiry_time = user_data.get("expiry_time")
+            if expiry_time is None:
+                # User previously used the free trial, but it has ended.
+                return False
+            elif isinstance(expiry_time, datetime.datetime) and datetime.datetime.now() <= expiry_time:
+                return True
+            else:
+                await self.premium.update_one({"id": user_id}, {"$set": {"expiry_time": None}})
+        return False
+
+    async def total_premium_users_count(self):
+        count = await self.premium.count_documents({})
+        return count
+
+    async def get_all_premium_users(self):
+        all_premium_users = self.premium.find({})
+        return all_premium_users
+        
 db = Database(Config.DB_URL, Config.DB_NAME)
 
-
-
-
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
