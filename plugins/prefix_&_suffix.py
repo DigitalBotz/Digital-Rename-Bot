642a2e51494cbee7b702d30282a6fb59c5d27a57
JayMahakal98 <136795836+JayMahakal98@users.noreply.github.com>
2024-05-02 07:17:23 +0545

    Create prefix_&_suffix.py

---
+++ plugins/prefix_&_suffix.py
@@ -0,0 +1,71 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+# Special Thanks To @ReshamOwner
+
+from pyrogram import Client, filters, enums
+from helper.database import db
+
+# prefix commond ✨
+@Client.on_message(filters.private & filters.command('set_prefix'))
+async def add_prefix(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Rkn_Bots`**")
+    prefix = message.text.split(" ", 1)[1]
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_prefix(message.from_user.id, prefix)
+    await RknDev.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('del_prefix'))
+async def delete_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if not prefix:
+        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_prefix(message.from_user.id, None)
+    await RknDev.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_prefix'))
+async def see_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if prefix:
+        await RknDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
+    else:
+        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+
+# SUFFIX COMMOND ✨
+@Client.on_message(filters.private & filters.command('set_suffix'))
+async def add_suffix(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Suffix__\n\nExᴀᴍᴩʟᴇ:- `/set_suffix @Rkn_Bots`**")
+    suffix = message.text.split(" ", 1)[1]
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_suffix(message.from_user.id, suffix)
+    await RknDev.edit("__**✅ ꜱᴜꜰꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('del_suffix'))
+async def delete_suffix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await db.get_suffix(message.from_user.id)
+    if not suffix:
+        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+    await db.set_suffix(message.from_user.id, None)
+    await RknDev.edit("__**❌️ ꜱᴜꜰꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_suffix'))
+async def see_suffix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await db.get_suffix(message.from_user.id)
+    if suffix:
+        await RknDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
+    else:
+        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+# Special Thanks To Snowball & @ReshamOwner
