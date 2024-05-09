40811ebe60fcfb06eb1f92eeb02b6144887521ef
JayMahakal98 <136795836+JayMahakal98@users.noreply.github.com>
2024-05-09 14:21:27 +0545

    Update To V3.0.0 🔥🔥

---
+++ plugins/__init__.py
@@ -0,0 +1,52 @@
+#  Telegram MTProto API Client Library for Pyrogram
+#  Copyright (C) 2017-present RknDeveloper <https://github.com/RknDeveloper>
+#
+"""
+Apache License 2.0
+Copyright (c) 2022 @RknDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Telegram Link : https://t.me/RknDeveloper 
+Repo Link : https://github.com/RknDeveloper/RknFileRenameBot
+License Link : https://github.com/RknDeveloper/Rkn-rename-bot-V3/blob/main/LICENSE
+"""
+__name__ = "Rename-Bot"
+__version__ = "3.0.0"
+__license__ = " Apache License, Version 2.0"
+__copyright__ = "Copyright (C) 2022-present RknDeveloper <https://github.com/RknDeveloper>"
+
+# main working files 
+# - bot.py
+# - web_support.py
+# - plugins/
+# - start_&_cb.py
+# - Force_Sub.py
+# - admin_panel.py
+# - file_rename.py
+# - metadata.py
+# - prefix_&_suffix.py
+# - thumb_&_cap.py
+# - config.py
+# - utils.py
+# - database.py
+
+# bot run files
+# - bot.py
+# - Procfile
+# - Dockerfile
+# - requirements.txt
+# - runtime.txt
