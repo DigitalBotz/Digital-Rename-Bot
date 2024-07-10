de6ec558bcd41b852c8090e1a6956dc981674219
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-07-10 23:42:29 +0545

    Updated DigitalRenameBot v3.0.3 ...

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -24,10 +24,11 @@ Telegram Link : https://t.me/RknDeveloper
 Repo Link : https://github.com/RknDeveloper/RknFileRenameBot
 License Link : https://github.com/RknDeveloper/Rkn-rename-bot-V3/blob/main/LICENSE
 """
-__name__ = "Rename-Bot"
-__version__ = "3.0.2"
+__name__ = "Digital-Rename-Bot"
+__version__ = "3.0.3"
 __license__ = " Apache License, Version 2.0"
 __copyright__ = "Copyright (C) 2022-present RknDeveloper <https://github.com/RknDeveloper>"
+__developer__ = "RknDeveloper <https://github.com/RknDeveloper>"
 
 # main working files 
 # - bot.py
