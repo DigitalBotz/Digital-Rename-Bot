f5f25aea07d0f5c2ab7e9be5a1e7496a6d2f31d7
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-08-22 22:33:03 +0545

    fixed: database...

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -1,12 +1,12 @@
 # (c) @RknDeveloperr
 # Rkn Developer 
 # Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
 # Update Channel @Digital_Botz & @DigitalBotz_Support
 
 from pyrogram import Client, filters 
-from helper.database import db
+from helper.database import digital_botz
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
@@ -14,22 +14,22 @@ async def add_caption(client, message):
     if len(message.command) == 1:
        return await rkn.edit("**__Gɪᴠᴇ Tʜᴇ Cᴀᴩᴛɪᴏɴ__\n\nExᴀᴍᴩʟᴇ:- `/set_caption {filename}\n\n💾 Sɪᴢᴇ: {filesize}\n\n⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`**")
     caption = message.text.split(" ", 1)[1]
-    await db.set_caption(message.from_user.id, caption=caption)
+    await digital_botz.set_caption(message.from_user.id, caption=caption)
     await rkn.edit("__**✅ Cᴀᴩᴛɪᴏɴ Sᴀᴠᴇᴅ**__")
    
 @Client.on_message(filters.private & filters.command(['del_caption', 'delete_caption', 'delcaption']))
 async def delete_caption(client, message):
     rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await digital_botz.get_caption(message.from_user.id)  
     if not caption:
        return await rkn.edit("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
-    await db.set_caption(message.from_user.id, caption=None)
+    await digital_botz.set_caption(message.from_user.id, caption=None)
     await rkn.edit("__**❌️ Cᴀᴩᴛɪᴏɴ Dᴇʟᴇᴛᴇᴅ**__")
                                        
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
 async def see_caption(client, message):
     rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await digital_botz.get_caption(message.from_user.id)  
     if caption:
        await rkn.edit(f"**Yᴏᴜ'ʀᴇ Cᴀᴩᴛɪᴏɴ:-**\n\n`{caption}`")
     else:
@@ -38,7 +38,7 @@ async def see_caption(client, message):
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 async def viewthumb(client, message):
     rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
-    thumb = await db.get_thumbnail(message.from_user.id)
+    thumb = await digital_botz.get_thumbnail(message.from_user.id)
     if thumb:
         await client.send_photo(chat_id=message.chat.id, photo=thumb)
         await rkn.delete()
@@ -48,9 +48,9 @@ async def viewthumb(client, message):
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delete_thumb', 'delthumb']))
 async def removethumb(client, message):
     rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
-    thumb = await db.get_thumbnail(message.from_user.id)
+    thumb = await digital_botz.get_thumbnail(message.from_user.id)
     if thumb:
-        await db.set_thumbnail(message.from_user.id, file_id=None)
+        await digital_botz.set_thumbnail(message.from_user.id, file_id=None)
         await rkn.edit("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
         return
     await rkn.edit("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__")
@@ -59,12 +59,12 @@ async def removethumb(client, message):
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     rkn = await message.reply_text("__**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**__")
-    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await digital_botz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await rkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
 
 # (c) @RknDeveloperr
 # Rkn Developer 
 # Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
 # Update Channel @Digital_Botz & @DigitalBotz_Support
